f760270c2cebb89b20569666b9ac25fb2be356f2
dxxxxy <introsdream@gmail.com>
2022-12-03 19:57:25 -0500

    initial commit: day 1 part 1

---
+++ 2022/Day 1 - Calorie Counting/index.js
@@ -0,0 +1,34 @@
+//import
+const fs = require("fs")
+
+//get input
+const input = fs.readFileSync("input.txt", "utf8")
+
+//separate input into array
+const calories = input.split("\n").map(calorie => calorie.replace("\r", ""))
+
+//init elves array
+const elves = []
+let tempCalories = 0
+
+//for each calorie
+calories.forEach(calorie => {
+    //if calorie is not a number (empty line), then his inventory ends and we add him to the elves array
+    if (Number.isNaN(parseInt(calorie))) {
+        elves.push(tempCalories)
+        tempCalories = 0
+        return
+    }
+
+    //else keep counting
+    tempCalories += parseInt(calorie)
+})
+
+//get max of array
+const max = Math.max(...elves)
+
+//get elf who has max 
+const elf = elves.indexOf(max) + 1
+
+//print
+console.log(`Elf ${elf} has the most calories: ${max}`)
